720fc22c7b993d37b27e9c91f0f7bc5a4182c76e
ElyPoli <itsos.poli@gmail.com>
2023-07-05 17:28:04 +0200

    start exercise dadi

--- js/dadi.js
+++ js/dadi.js
@@ -5,3 +5,19 @@ Stabilire il vincitore, in base a chi fa il punteggio più alto.
 
 BONUS: far inserire 3 numeri all’utente, generare per il pc 3 numeri, e dopo aver calcolato la somma dei numeri dell’utente e dei numeri del pc, capire chi ha vinto in base alla somma maggiore.
 */
+
+// Dichiaro variabili
+const userNumber = Math.floor(Math.random() * (6 - 1)) + 1;
+const pcNumber = Math.floor(Math.random() * (6 - 1)) + 1;
+
+console.log("numero utente " + userNumber);
+console.log("numero pc " + pcNumber);
+
+// Confronto i due numeri per stabilire il vincitore
+if (userNumber > pcNumber) {
+    console.log("Ha vinto l'utente");
+} else if (userNumber < pcNumber) {
+    console.log("Ha vinto il computer");
+} else {
+    console.log("Parità");
+}
