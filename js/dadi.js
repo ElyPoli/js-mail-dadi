4d374f058ee18a3ad8f8339e7097ce0c81a02fb1
ElyPoli <itsos.poli@gmail.com>
2023-07-05 14:50:27 +0200

    created files

---
+++ js/dadi.js
@@ -0,0 +1,7 @@
+/*
+Gioco dei dadi
+Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+BONUS: far inserire 3 numeri all’utente, generare per il pc 3 numeri, e dopo aver calcolato la somma dei numeri dell’utente e dei numeri del pc, capire chi ha vinto in base alla somma maggiore.
+*/
---
+++ js/mail.js
@@ -0,0 +1,8 @@
+/*
+Mail:
+- chiedi all’utente la sua email,
+- controlla che sia nella lista di chi può accedere,
+- stampa un messaggio appropriato sull’esito del controllo.
+- Usiamo un input in html per inserire la mail da cercare.
+*/
+
